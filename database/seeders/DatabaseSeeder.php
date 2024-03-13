d7264fa6ee114a7b3d484a8a152d6a8ba068d68c
Shenal Fernando <developershenal@gmail.com>
2024-03-14 05:22:56 +0530

    update seeders for production

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             CategoriesTableSeeder::class,
             PaymentMethodsTableSeeder::class,
-            PaymentsTableSeeder::class,
-            IncomesTableSeeder::class,
+            //PaymentsTableSeeder::class,
+            //IncomesTableSeeder::class,
         ]);
     }
 }
