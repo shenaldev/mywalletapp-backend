a6efbea9d1b824e0103fddfa7cc6db27dcbbb63c
Shenal Fernando <developershenal@gmail.com>
2024-10-13 02:41:08 +0530

    upgrade to laravel 11

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -2,36 +2,19 @@
 
 namespace Tests\Feature\Auth;
 
-use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AuthenticationTest extends TestCase
 {
-    use RefreshDatabase;
-
-    public function test_users_can_authenticate_using_the_login_screen(): void
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
     {
-        $user = User::factory()->create();
+        $response = $this->get('/');
 
-        $response = $this->post('/login', [
-            'email' => $user->email,
-            'password' => 'password',
-        ]);
-
-        $this->assertAuthenticated();
-        $response->assertNoContent();
-    }
-
-    public function test_users_can_not_authenticate_with_invalid_password(): void
-    {
-        $user = User::factory()->create();
-
-        $this->post('/login', [
-            'email' => $user->email,
-            'password' => 'wrong-password',
-        ]);
-
-        $this->assertGuest();
+        $response->assertStatus(200);
     }
 }
