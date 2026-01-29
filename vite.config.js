8b3464616310c1216acd35d6d21b5d879705f2aa
Shenal Fernando <developershenal@gmail.com>
2026-01-30 00:28:31 +0530

    install tailwind with vite

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+    plugins: [laravel(["resources/css/app.css"], tailwindcss())],
+});
